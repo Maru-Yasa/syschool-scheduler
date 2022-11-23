067ef59cbcef470735fb3c6f8a0644dc5b886a7d
Maru-Yasa <marufilyasa13@gmail.com>
2022-11-23 23:23:46 +0700

    🔨 UPDATE: dynamic title

--- resources/views/layouts/preview.blade.php
+++ resources/views/layouts/preview.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Jadwal Pelajaran</title>
+    <title>Jadwal Pelajaran | {{ $setting_umum->nama_sekolah }}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
